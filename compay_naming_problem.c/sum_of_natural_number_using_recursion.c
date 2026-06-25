efaa1285e2c0f6b1b3701a3d1bbb48accf7006ae
Nitya Sharma <advocatemanjunitya@gmail.com>
2026-06-25 12:44:39 +0530

    questions

---
+++ compay_naming_problem.c/sum_of_natural_number_using_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sum(int n)
+{
+    if (n == 0)
+        return 0;
+    else
+        return n + sum(n - 1);
+}
+
+int main()
+{
+    int num, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    result = sum(num);
+
+    printf("Sum of first %d natural numbers = %d", num, result);
+
+    return 0;
+}
